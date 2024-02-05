c738dc5f383e6771f431b7288852f306aec6299e
Yanboww <wywkaixin10@gmail.com>
2024-02-05 18:11:31 -0500

    Stopped using java.util.*

--- src/MovieCollection.java
+++ src/MovieCollection.java
@@ -2,7 +2,10 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Array;
-import java.util.*;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class MovieCollection
 {
